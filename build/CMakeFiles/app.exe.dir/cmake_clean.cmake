80cff58a5c2f3c3c4de62f6b435aee4c55a2f1e9
senyich <arseniyneshetin@gmail.com>
2025-09-05 00:25:48 +0700

    upd

---
+++ build/CMakeFiles/app.exe.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/app.exe.dir/src/source/main.cpp.o"
+  "CMakeFiles/app.exe.dir/src/source/main.cpp.o.d"
+  "CMakeFiles/app.exe.dir/src/source/solution1.cpp.o"
+  "CMakeFiles/app.exe.dir/src/source/solution1.cpp.o.d"
+  "app.exe"
+  "app.exe.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/app.exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
